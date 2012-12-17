1c2305b9c08d5df1ed310cd96bd8a6354845bef4
Victor Miguez <victor@victor-curro-laptop.(none)>
2012-12-17 22:36:51 +0100

    deuce and advantage cases

--- spec/tennis_spec.rb
+++ spec/tennis_spec.rb
@@ -19,14 +19,65 @@ describe 'tennis game' do
 	describe 'scoring' do
 		describe 'player 1 first' do
 			before(:each) do
-				game.score_1 end
-			it 'player 1 scores first then score should be 15-0' do
-			
+				game.score_1 
+      end
+			it 'then score should be 15-0' do
 				game.print_score.should eq '15-0'
 			end
-			it 'player 1 scores first then his score should be 15' do
+			it 'then his score should be 15' do
 				game.print_score_1.should eq '15'
 			end
+      
+      describe 'then player 2 scores' do
+        before(:each) do
+          game.score_2 
+        end
+        it 'then score should be 15-15' do
+          game.print_score.should eq '15-15'
+        end
+        it 'then his score should be 15' do
+          game.print_score_2.should eq '15'
+        end
+      end
 		end
+    describe 'deuce' do
+      before(:each) do
+        (0..2).each do 
+          game.score_1 
+          game.score_2
+        end
+      end
+      it 'then score is DEUCE' do
+        game.print_score.should eq 'DEUCE'
+      end
+      describe 'player 1 scores' do
+        before(:each) do
+          game.score_1
+        end
+        it 'then score is ADV-40' do
+          game.print_score.should eq 'ADV-40'
+        end
+      end
+      describe 'player 2 scores' do
+        before(:each) do
+          game.score_2
+        end
+        it 'then score is 40-ADV' do
+          game.print_score.should eq '40-ADV'
+        end
+      end
+     end
 	end
+
+  describe 'player 2 scores first' do
+    before(:each) do
+      game.score_2 end
+    it 'then score should be 0-15' do
+      game.print_score.should eq '0-15'
+    end
+    it 'then his score should be 15' do
+      game.print_score_2.should eq '15'
+    end
+  end
+
 end
--- src/tennis.rb
+++ src/tennis.rb
@@ -3,26 +3,34 @@ class Tennis
 	def initialize
 		@score_printer = TennisScorePrinter.new
 		@score_1 = 0
+		@score_2 = 0
 	end
-	def print_score
-		@score_printer.print(@score_1) + '-0'
+
+  def print_score
+    @score_1 == 3 && @score_2 == 3? 'DEUCE' : @score_printer.print(@score_1, @score_2) + '-' + @score_printer.print(@score_2, @score_1)
 	end
 
 	def print_score_1
-		@score_printer.print @score_1	
+		@score_printer.print(@score_1, @score_2)
 	end
 
 	def print_score_2
-		'0'
+		@score_printer.print(@score_2, @score1)	
 	end
 
 	def score_1
 		@score_1 += 1
 	end
+
+	def score_2
+		@score_2 += 1
+	end
+
 end
+
 class TennisScorePrinter
 	SCORES = %w{0 15 30 40}
-	def print(score)
-		SCORES[score]	
+	def print(score, other)
+    score == 4 && other == 3? 'ADV' : SCORES[score]	
 	end
 end
